c8f09b077181b5ce4164df5faa81c7e1a9b8300a
Gazha Alexandr Vladimirovich <gazha.alexandr93@gmail.com>
2021-01-21 16:42:39 +0200

    Now GCD can work with list nums

--- Algorithms/Fx.cs
+++ Algorithms/Fx.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,30 +11,35 @@ namespace Algorithms
         /// <summary>
         /// If one of args is 0, func return 0
         /// </summary>
-        /// <param name="f">First num</param>
-        /// <param name="s">Second num</param>
-        /// <returns>GCD for first and second num</returns>
-        public static ulong GCD(ulong f, ulong s)
+        /// <param name="n">List of nums, which great then 0</param>
+        /// <returns>GCD for list n</returns>
+        public static ulong GCD(List<ulong> n)
         {
-            return Task.Run(() => gcd(f, s)).Result;
+            return Task.Run(() => gcd(n)).Result;
 
-            static ulong gcd(ulong f, ulong s)
+            static ulong gcd(List<ulong> n)
             {
+                //Core
+                if (n.Count > 2)
+                    n[1] = gcd(n.Where(w => w != n[0]).ToList());
+
                 //Protector
-                if (f == 0 || s == 0)
+                if (n[0] == 0 || n[1] == 0)
                     return 0;
 
                 //Protector
-                if (s > f)
+                if (n[1] > n[0])
                 {
-                    f = f + s;
-                    s = f - s;
-                    f = f - s;
+                    n[0] = n[0] + n[1];
+                    n[1] = n[0] - n[1];
+                    n[0] = n[0] - n[1];
                 }
 
-                ulong r = f % s;
+                ulong r = n[0] % n[1];
 
-                return r == 0 ? s : gcd(s, r);
+                return r == 0 ? n[1] : gcd(new List<ulong>() { n[1], r });
+
+
             }
         }
     }
